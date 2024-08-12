de98ff47c317d67884b0fd1fc688262bac3eb043
Zahra11Mosbal11 <zahramosbal@gmail.com>
2024-08-12 18:12:49 +0000

    test

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+"""DB module.
+"""
+from sqlalchemy import create_engine, tuple_
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.orm.session import Session
+
+from user import Base, User
+
+
+class DB:
+    """DB class.
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance.
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object.
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """Adds a new user to the database.
+        """
+        new_user = User(email=email, hashed_password=hashed_password)
+
+        self._session.add(new_user)
+        self._session.commit()
+
+        return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Find user
+        """
+        try:
+            return self._session.query(User).filter_by(**kwargs).one()
+        except NoResultFound:
+            raise NoResultFound("No user found matching the criteria")
+        except InvalidRequestError:
+            raise InvalidRequestError("Invalid query arguments provided")
