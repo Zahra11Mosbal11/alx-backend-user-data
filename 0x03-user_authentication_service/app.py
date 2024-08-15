69d98431ac7cb385339c32d8831317e1865844ed
Zahra11Mosbal11 <zahramosbal@gmail.com>
2024-08-15 05:21:14 +0000

    try

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -50,7 +50,7 @@ def logout():
 @app.route("/profile", methods=["GET"])
 def profile():
     """User profile"""
-    ession_id = request.cookies.get("session_id")
+    session_id = request.cookies.get("session_id")
     user = AUTH.get_user_from_session_id(session_id)
     if user is None:
         abort(403)
